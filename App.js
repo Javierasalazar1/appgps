29ff6aea78681bda37ff2a870375fe66261fe47a
Javi <105263003+Javierasalazar1@users.noreply.github.com>
2024-06-17 23:59:34 -0400

    j
    gghghj

--- App.js
+++ App.js
@@ -1,10 +1,86 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, query, where, getDocs, collection } from 'firebase/firestore';
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDK71FGurfMwk2XbZ3UwzdC-uTHegEZkj4",
+  authDomain: "gps2024-119de.firebaseapp.com",
+  databaseURL: "https://gps2024-119de-default-rtdb.firebaseio.com",
+  projectId: "gps2024-119de",
+  storageBucket: "gps2024-119de.appspot.com",
+  messagingSenderId: "816992076661",
+  appId: "1:816992076661:web:e715cd65134c743dcc493c",
+  measurementId: "G-VXR027HFXL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      if (!email || !password) {
+        setErrorMessage('Por favor, ingresa el correo y la contraseña.');
+        return;
+      }
+
+      // Consultar Firestore para obtener el usuario
+      const q = query(collection(firestore, 'users'), where('email', '==', email));
+      const querySnapshot = await getDocs(q);
+      
+      // Verificar si el usuario existe y la contraseña es correcta
+      if (!querySnapshot.empty) {
+        const userData = querySnapshot.docs[0].data();
+        if (userData.password === password) {
+          setUserEmail(email); // Establecer el email del usuario
+          setErrorMessage('');
+        } else {
+          setErrorMessage('Contraseña incorrecta');
+        }
+      } else {
+        setErrorMessage('Usuario no encontrado');
+      }
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+      setErrorMessage('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/ubb.png')} style={styles.logo} />
+      <Text style={styles.title}>Ingresar</Text>
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+      {userEmail ? (
+        <Text style={styles.loggedText}>Logged in as {userEmail}</Text>
+      ) : null}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +92,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    width: '100%',
+    padding: 8,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: '#FFD700', // Color amarillo
+    padding: 10,
+    marginVertical: 8,
+    borderRadius: 4,
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  loggedText: {
+    marginTop: 8,
   },
 });
